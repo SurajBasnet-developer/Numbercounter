1c08499b8d8f504e0c8569f156d8421e12a65169
Suraj <surajbasnet449@gmail.com>
2023-02-04 16:54:55 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>This is a Number Counting App</h3>
+      <h2>{number}</h2>
+      <button onClick={() => setNumber(number + 1)} className="button">
+        Add
+      </button>
+      <button onClick={() => setNumber(number - 1)} className="button">
+        Down
+      </button>
+      <button onClick={() => setNumber(0)} className="button">
+        Reset
+      </button>
     </div>
   );
-}
+};
 
 export default App;
